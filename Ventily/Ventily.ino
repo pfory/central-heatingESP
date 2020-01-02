84095d57d647726d7da22adde8c2f124634f9149
Petr <pfory@seznam.cz>
2020-01-02 14:29:07 +0100

    +

--- Ventily/Ventily.ino
+++ Ventily/Ventily.ino
@@ -6,20 +6,34 @@ PCF8574 PCF_02(0x21);
 
 // Rotační enkodér KY-040
 
-// proměnné pro nastavení propojovacích pinů
-// int pinCLK = D5;
-// int pinDT  = D6;
-// int pinSW  = D7;
-int pinCLK = 0;
-int pinDT  = 1;
+//PCF-01
+int pinCLKValve1        = 0;
+int pinDTValve1         = 1;
+int pinCLKValve2        = 2;
+int pinDTValve2         = 3;
+int pinCLKValve3        = 4;
+int pinDTValve3         = 5;
+int pinCLKValve4        = 6;
+int pinDTValve4         = 7;
+//PCF-02        
+int pinCLKValve5        = 0;
+int pinDTValve5         = 1;
+int pinPolarityRelay    = 2;
+int pinrelayValve1      = 3;
+int pinrelayValve2      = 4;
+int pinrelayValve3      = 5;
+int pinrelayValve4      = 6;
+int pinrelayValve5      = 7;
 
 
-// proměnné pro uložení pozice a stavů pro určení směru
-// a stavu tlačítka
-int poziceEnkod = 0;
-int poziceEnkodOld = 0;
-int stavPred;
-int stavCLK;
+int poziceEnkod         = 0;
+int poziceEnkodOld      = 0;
+int stavPredValve1      = 0;
+int stavPredValve2      = 0;
+int stavPredValve3      = 0;
+int stavPredValve4      = 0;
+int stavPredValve5      = 0;
+int stavCLK             = 0;
 
 #include <timer.h>
 auto timer = timer_create_default(); // create a timer with default settings
@@ -28,30 +42,16 @@ Timer<> default_timer; // save as above
 void setup() {
   Serial.begin(115200);
   Serial.println("Ventil");
-  // nastavení propojovacích pinů jako vstupních
-  //pinMode(pinCLK, INPUT);
-  //pinMode(pinDT, INPUT);
-  
-  // pinMode(D1, OUTPUT);
-  // pinMode(D2, OUTPUT);
-  // pinMode(D3, OUTPUT);
-  // digitalWrite(D1, HIGH);
-  // digitalWrite(D2, HIGH);
-  // digitalWrite(D3, HIGH);
   
   PCF_01.begin();
   PCF_02.begin();
 
-  // Serial.println(PCF_01.read(0));
-  // Serial.println(PCF_01.read(1));
-  // Serial.println(PCF_01.read(2));
-  // Serial.println(PCF_01.read(3));
-  // Serial.println(PCF_01.read(4));
 
-  //stavPred = digitalRead(pinCLK);   
-  stavPred = PCF_01.read(pinCLK);
-  // Serial.println(PCF_01.read(pinCLK));
-  // Serial.println(PCF_01.read(pinDT));
+  stavPredValve1 = PCF_01.read(pinCLKValve1);
+  stavPredValve2 = PCF_01.read(pinCLKValve2);
+  stavPredValve3 = PCF_01.read(pinCLKValve3);
+  stavPredValve4 = PCF_01.read(pinCLKValve4);
+  stavPredValve5 = PCF_02.read(pinCLKValve5);
   
   timer.every(800, checkStatus);
 }
@@ -59,41 +59,29 @@ void setup() {
 void loop() {
   timer.tick(); // tick the timer
 
-  // Serial.println(PCF_01.read(pinCLK));
-  // Serial.println(PCF_01.read(pinDT));
-
   if (Serial.available() > 0) {
-    // read the incoming byte:
     int incomingByte = Serial.read();
 
-    // say what you got:
     Serial.print("I received: ");
     Serial.println(incomingByte, DEC);
     if (incomingByte == 48) {
       //close
       Serial.print("Zavirani ventilu ");
-      // digitalWrite(D3, HIGH);
-      // digitalWrite(D1, HIGH);
-      // digitalWrite(D2, HIGH);
       poziceEnkod = 0;
+      stavPred = getStavPred(1);
     } else if (incomingByte == 49) {
       //open
       Serial.print("Otevirani ventilu ");
-      // digitalWrite(D3, HIGH);
-      // digitalWrite(D1, LOW);
-      // digitalWrite(D2, LOW);
       poziceEnkod = 0;
+      stavPred = getStavPred(1);
     } else {
-      // digitalWrite(D3, LOW);
     }
    }
     
     
-  //stavCLK = digitalRead(pinCLK);
-  stavCLK = PCF_01.read(pinCLK);
+  stavCLK = getStavCLK(1);
   if (stavCLK != stavPred) {
-    //if (digitalRead(pinDT) != stavCLK) {
-    if (PCF_01.read(pinDT) != stavCLK) {
+    if (PCF_01.read(pinDTValve1) != stavCLK) {
       poziceEnkod++;
     } else {
       poziceEnkod--;
@@ -104,15 +92,27 @@ void loop() {
 }
 
 bool checkStatus(void *) {
-  //Serial.println(poziceEnkod);
-  //Serial.println(poziceEnkodOld);
   if (poziceEnkod == poziceEnkodOld) {
-    // digitalWrite(D3, LOW);
-    // digitalWrite(D1, HIGH);
-    // digitalWrite(D2, HIGH);
   } else {
     poziceEnkodOld = poziceEnkod;
-    //Serial.println("Vypnuti rele");
   }
   return true;
+}
+
+int getStavCLK(int valve) {
+  if (valve == 1) { return PCF_01.read(pinCLKValve1); }
+  else if (valve == 2) { return PCF_01.read(pinCLKValve2); }
+  else if (valve == 3) { return PCF_01.read(pinCLKValve3); }
+  else if (valve == 4) { return PCF_01.read(pinCLKValve4); }
+  else if (valve == 5) { return PCF_02.read(pinCLKValve5); }
+  else { return 0; }
+}
+
+int getStavPred(int valve) {
+  if (valve == 1) { return stavPredValve1; }
+  else if (valve == 2) { return stavPredValve2; }
+  else if (valve == 3) { return stavPredValve3; }
+  else if (valve == 4) { return stavPredValve4; }
+  else if (valve == 5) { return stavPredValve5; }
+  else { return 0; }
 }
